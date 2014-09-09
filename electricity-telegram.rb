c893936e762ab07df8fce4ef5a919a231638d99d
alagunambi <alagunambiwelkin@gmail.com>
2014-09-09 13:16:16 +0530

    Inital commit of power-cut alert scripts for telegram

---
+++ electricity-telegram.rb
@@ -0,0 +1,27 @@
+#!/bin/ruby
+# Script to check for power shutdown notifications in EB site and report to UDProducts telegram group.
+
+# Libraries
+require 'nokogiri'
+require 'open-uri'
+
+# Primary variables
+source = "http://tneb.tnebnet.org/cpro/today.html"
+area = "Velacherry"
+
+# Check for necessary executable
+# exec('[ -f /bin/telegram-shell ] && echo "Telegram-shell found." || echo "Telegram-shell is Not Found at /bin :("')
+
+# Scarpping source url 
+doc = Nokogiri::HTML(open(source))
+
+# Checking area name in scrapped web page and communicating information
+if doc.text.match("/#{area}/i")
+  puts "Power cut in #{area.to_s}"
+  exec('telegram-shell UDProducts "Power cut in ' + area.to_s + ', please verify at ' + source.to_s + '"')
+else
+#  telegram-shell UDProducts "#{area}: No power cut today!"
+  exec('telegram-shell UDProducts "' + area.to_s + ': No power cut today!"')
+end
+
+return 0
